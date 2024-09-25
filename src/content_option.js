c16552f63525d86c487f543100d9fdf3eddfc6c5
vn17 <vyshakh.nambiar@gmail.com>
2024-09-25 01:18:01 +0000

    Name change

--- src/content_option.js
+++ src/content_option.js
@@ -5,7 +5,7 @@ const meta = {
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Vyshakh",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
